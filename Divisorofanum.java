9c19a39cb148092a6760bc449faad796c7c8417d
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-07-17 11:17:47 +0530

    divisors of given number problem solved

--- Divisorofanum.java
+++ Divisorofanum.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Divisorofanum {
-    public static int findDivisorOfGivenNum(int num) {
+    public static void findDivisors(int N) {
+        System.out.println("Divisors of" + N + "are: ");
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                System.out.print(i + " ");
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of which you want all the divisors of it to be printed: ");
+        int N = sc.nextInt();
+        findDivisors(N);
+        sc.close();
 
     }
 }
