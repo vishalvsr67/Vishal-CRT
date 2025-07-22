d73d4374613377f2359bd77693c57026f0b055cc
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-07-22 12:17:27 +0530

    to check if the array is sorted or not problem is solved

---
+++ checkifarrsortedbrute.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class checkifarrsortedbrute {
+    static boolean checkIfSorted(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(checkIfSorted(arr, n));
+        sc.close();
+    }
+
+}
